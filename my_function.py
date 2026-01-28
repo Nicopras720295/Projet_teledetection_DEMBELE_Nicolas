3acee648f969ebb232bbca961654d199cb471e7c
nicopras72 <nicolas.dembele@etu.univ-tlse2.fr>
2026-01-28 15:40:02 +0000

    Ajout du masque pour découper l'image final

--- my_function.py
+++ my_function.py
@@ -4,7 +4,7 @@ import pandas as pd
 from osgeo import gdal
 
 def rasterisation(my_folder, in_vector, ref_image, out_image, field_name, sptial_resolution, xmin, ymin, xmax, ymax): 
-    """Version exacte de Matthieu GOUBERT"""
+
     cmd_pattern = ("gdal_rasterize -a {field_name} "
                "-tr {sptial_resolution} {sptial_resolution} "
                "-te {xmin} {ymin} {xmax} {ymax} -ot Byte -of GTiff "
@@ -26,6 +26,6 @@ def calculate_nari_safe(b3, b5):
     return nari
 
 def report_to_df(report_dict):
-    """Utilitaire pour transformer les résultats en tableau (Méthode Djiby)"""
+    """Utilitaire pour transformer les résultats en tableau"""
     df = pd.DataFrame(report_dict).transpose()
     return df.loc[df.index.isin(['2', '3', '4'])]
